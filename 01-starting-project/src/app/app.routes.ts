2df4595a3c127b7e25bd3e1412160bbbc3c6ef14
Lucas Landivar <landivarlucas67@gmail.com>
2025-03-27 20:37:19 -0300

    Resolving Route-related Dynamic Data

--- 01-starting-project/src/app/app.routes.ts
+++ 01-starting-project/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
-import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
+import {
+  resolveUserName,
+  UserTasksComponent,
+} from './users/user-tasks/user-tasks.component';
 import { routes as userRoutes } from './users/users.routes';
 import { NotFoundComponent } from './not-found/not-found.component';
 
@@ -17,6 +20,9 @@ export const routes: Routes = [
     children: userRoutes,
     data: {
       message: 'Hello!',
+    },
+    resolve: {
+      userName: resolveUserName,
     },
   },
   {
